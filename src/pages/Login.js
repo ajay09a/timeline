8380c16d861d67c717434604a462ca4bcd800cfa
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-05-26 12:30:07 +0530

    created auth provider

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,59 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
 import styles from '../styles/login.module.css';
+import { login } from '../api';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoggingIn(true);
+
+    if (!email || !password) {
+      return toast.warn('Please enter both email and password')
+    }
+
+    const response = await login(email, password);
+
+    if (response.success) {
+      toast.success('Successfully logged in');
+    } else {
+      toast.error(response.message);
+    }
+
+    setLoggingIn(false);
+  };
+
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>Log In</span>
 
       <div className={styles.field}>
-        <input type="email" placeholder="Email" required />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
 
       <div className={styles.field}>
-        <input type="password" placeholder="Paasword" required />
+        <input
+          type="password"
+          placeholder="Paasword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
       <div className={styles.field}>
-        <button>Log In</button>
+        <button disabled={loggingIn}>
+          {loggingIn ? 'Logging in...' : 'Log In'}
+        </button>
       </div>
     </form>
   );
---
+++ src/providers/AuthProvider.js
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+const initialState = {
+    user: null,
+    login: ()=>{},
+    logout: ()=>{},
+    loading: true,
+}
+
+export const AuthContext = createContext(initialState);
+
+export const AuthProvider = ({children}) =>{
+    const auth = useProviderAuth();
+    return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
+}
