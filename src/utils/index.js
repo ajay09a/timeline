2b4f47e36770226384bd2be2d9575eef1142ba7f
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-05-26 14:05:59 +0530

    get from body

--- src/utils/index.js
+++ src/utils/index.js
@@ -27,3 +27,16 @@ export const removeItemFromLocalStorage = (value, key) => {
 
   localStorage.removeItem(key);
 };
+
+export const getFormBody = (params) => {
+  let formBody = [];
+
+  for (let property in params) {
+    let encodedKey = encodeURIComponent(property); // 'user name' => 'user%20name'
+    let encodedValue = encodeURIComponent(params[property]); // aakash 123 => aakash%2020123
+
+    formBody.push(encodedKey + '=' + encodedValue);
+  }
+
+  return formBody.join('&'); // 'username=aakash&password=123213'
+};
