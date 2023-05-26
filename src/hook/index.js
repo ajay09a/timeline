b53d4ce582a3581f95d84f5ae6c2ee7113616ca0
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-05-26 14:03:49 +0530

    set item in local storage

--- src/hook/index.js
+++ src/hook/index.js
@@ -1,3 +1,15 @@
 export const useProviderAuth = ()=>{
-    
+    const [user, setUser] = uUeState(null);
+    const [loading, setLoading] = uUeState(true);
+
+    const login = (email, password)=>{};
+
+    const logout = ()=>{};
+
+    return {
+        user,
+        login,
+        logout,
+        loading,
+    }
 }
